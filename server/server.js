cb80a20faf8ecd79e15be2a435014d962d3d2f4e
Andrew <armcburn@gmail.com>
2022-03-09 16:02:05 -0500

    Config API: Postgress with Node Postgres

--- server/server.js
+++ server/server.js
@@ -1,15 +1,45 @@
 require("dotenv").config();
 const express = require("express")
+const db = require('./db') //this should automatically look for index.js file
+
 
 
 const app = express()
 
-app.get("/getbusinesses", (req, res) => {
-    res.status(200).json({
-        status: "success",
-        name: "mcdonalds",
-    })
+
+
+app.get("/api/v1/businesses", async (req, res) => {
+    
+
+
+    try {
+
+        const results = await db.query("SELECT * FROM businesses")
+
+        console.log(results)
+        
+        res.status(200).json({
+
+            status: "success",
+            results: results.rows.length,
+            data: {
+                restaurants: results.rows,
+            },
+
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+    
+
 })
+
+
+
+
+
+
 
 const port = process.env.PORT || 3001
 
